9efd4627fd613961304645712a1c7bb4665abebe
David Bjorgen <dbjorgen@gmail.com>
2018-11-06 10:12:42 -0500

    Additions

--- assets/js/teachturtles-min.js
+++
@@ -1,1 +0,0 @@
-function autoResizeDiv(){document.getElementById("tt-frame").style.height=window.innerHeight+"px"}(window.onresize=autoResizeDiv)();
--- assets/js/teachturtles.js
+++
@@ -1,5 +0,0 @@
-function autoResizeDiv() {
-    document.getElementById('tt-frame').style.height = window.innerHeight +'px';
-}
-window.onresize = autoResizeDiv;
-autoResizeDiv();
